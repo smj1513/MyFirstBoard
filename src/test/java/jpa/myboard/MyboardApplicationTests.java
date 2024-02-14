6cab049c455a1074fba995c616ba73200dc660ab
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-02-14 19:20:50 +0900

    게시판 2024-02-14

--- src/test/java/jpa/myboard/MyboardApplicationTests.java
+++ src/test/java/jpa/myboard/MyboardApplicationTests.java
@@ -1,16 +1,20 @@
 package jpa.myboard;
 
 import jpa.myboard.boardPost.domain.BoardPost;
+import jpa.myboard.boardPost.repository.BoardPostRepository;
 import jpa.myboard.comment.domain.Comment;
+import jpa.myboard.user.domain.Gender;
 import jpa.myboard.user.domain.SiteUser;
+import jpa.myboard.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class MyboardApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
